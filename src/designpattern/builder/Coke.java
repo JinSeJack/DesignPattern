4e47f31336b07e8ad98fc29ce847f9c7223377e9
jackw <wangjianming1994@163.com>
2018-04-02 20:44:39 +0800

    建造者模式：创建一个复杂对象时，可以通过一个创建者来管理组成复杂对象的简单对象，从而简化复杂对象的获取步骤，提高程序复用。示例中创建了一顿饭的创造者MealBuilder，通过它可以得到两种类型的事物套餐，套餐中的各个类型又是不同的，还需要获得他们的价格以及包装，通过MealBuilder可以统一处理了这一个过程，省去了后续的很多重复工作。另StringBuilder也是一个创造者模式的类产生器。

---
+++ src/designpattern/builder/Coke.java
@@ -0,0 +1,17 @@
+package designpattern.builder;
+
+public class Coke extends ColdDrink{
+
+	@Override
+	public String name() {
+		// TODO Auto-generated method stub
+		return "coke";
+	}
+
+	@Override
+	public float price() {
+		// TODO Auto-generated method stub
+		return 2.5f;
+	}
+
+}
---
+++ src/designpattern/builder/Pepsi.java
@@ -0,0 +1,17 @@
+package designpattern.builder;
+
+public class Pepsi extends ColdDrink{
+
+	@Override
+	public String name() {
+		// TODO Auto-generated method stub
+		return "pepsi";
+	}
+
+	@Override
+	public float price() {
+		// TODO Auto-generated method stub
+		return 3.0f;
+	}
+
+}
